cdd01ade6817e58261d792a93a77560e25033b5b
alvinshaita <alvin@kickback.space>
2022-10-29 20:56:16 +0300

    throw in tuple test, to_dict

--- tests.py
+++ tests.py
@@ -162,10 +162,25 @@ class TestAttriDict(unittest.TestCase):
 		self.assertEqual(type(att.two[0]), dict)
 		self.assertEqual(att.two[0], {"three": 333, "four": 444})
 
-	def test_to_dict_deep(self):
+	def test_to_dict_deep_list(self):
 		data = {"one": 111, "two": [{"three": 333, "four": 444}]}
 		att = attridict(data, True)
-		
+	
+		self.assertEqual(type(att.two[0]), attridict)
+		self.assertEqual(att.two[0], {"three": 333, "four": 444})
+		self.assertEqual(att.two[0].three, 333)
+
+		d_attr = att.to_dict()
+
+		with self.assertRaises(AttributeError) as ctx:
+			d_attr["two"][0].three
+		self.assertEqual(str(ctx.exception), "'dict' object has no attribute 'three'")
+
+
+	def test_to_dict_deep_tuple(self):
+		data = {"one": 111, "two": tuple(({"three": 333, "four": 444},))}
+		att = attridict(data, True)
+	
 		self.assertEqual(type(att.two[0]), attridict)
 		self.assertEqual(att.two[0], {"three": 333, "four": 444})
 		self.assertEqual(att.two[0].three, 333)
