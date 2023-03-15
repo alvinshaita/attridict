70f1b9a6c8e52b4219198eb9c8dbc42fda5b86c9
alvinshaita <alvinshaita@gmail.com>
2023-03-15 16:01:30 +0300

    modify versioning

--- attridict.py
+++ attridict.py
@@ -7,11 +7,10 @@ __author__	= "Alvin Shaita"
 __email__	= "alvinshaita@gmail.com"
 
 
-class AttriDictAttributes:
-	__version__ = "0.0.4"
-
-class AttriDict(dict, AttriDictAttributes):
+class AttriDict(dict):
 	'''AttriDict'''
+
+	__version__ = "0.0.4"
 
 	def __init__(self, data = {}):
 		super(type(self), self).__init__(data)
