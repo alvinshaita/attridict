437c89893c9d9b5d3ba1fc6fcfa64a1060d14c0c
Napat Ch <napat@thevcgroup.com>
2023-07-11 20:03:05 +0700

    reformat and make AttriDict pickable

--- attridict.py
+++ attridict.py
@@ -1,59 +1,62 @@
-'''
-	attridict.py:
-		Alvin Shaita <alvinshaita@gmail.com>
-'''
+"""
+    attridict.py:
+        Alvin Shaita <alvinshaita@gmail.com>
+"""
 
-__author__	= "Alvin Shaita"
-__email__	= "alvinshaita@gmail.com"
+__author__ = "Alvin Shaita"
+__email__ = "alvinshaita@gmail.com"
 
+import types
 from mixins import MapMixin
 
 
-__all__ = ["AttriDict"]
+class AttriDict(types.ModuleType):
+    def __call__(self, *args, **kwargs):
+        return _AttriDict(*args, **kwargs)
 
-class AttriDict(dict, MapMixin):
-	'''AttriDict'''
 
-	__version__ = "0.0.7"
+class _AttriDict(dict, MapMixin):
+    """AttriDict"""
 
-	__all__ = ["to_dict"]
+    __version__ = "0.0.7"
 
-	def __init__(self, *args, **kwargs):
-		if args and not isinstance(*args, dict):
-			raise AttributeError(
-				"non dict argument provided"
-			)
+    __all__ = ["to_dict"]
 
-		super(type(self), self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        if args and not isinstance(*args, dict):
+            raise AttributeError(
+                "non dict argument provided"
+            )
+        super().__init__(*args, **kwargs)
 
-	def _attrify(self, obj):
-		"""
-		Convert dict instance objects to attridict
-		"""
-		if isinstance(obj, dict):
-			obj = type(self)(obj)
-		elif isinstance(obj, (list, set, tuple)):
-			obj = type(obj)(self._attrify(i) for i in obj)
+    def _attrify(self, obj):
+        """
+        Convert dict instance objects to attridict
+        """
+        if isinstance(obj, dict):
+            obj = type(self)(obj)
+        elif isinstance(obj, (list, set, tuple)):
+            obj = type(obj)(self._attrify(i) for i in obj)
 
-		return obj
+        return obj
 
-	def _valid_key(self, key):
-		"""
-		Check if the key provided is a valid key
-		"""
-		if key in dir(self):
-			return False
+    def _valid_key(self, key):
+        """
+        Check if the key provided is a valid key
+        """
+        if key in dir(self):
+            return False
 
-		return True
+        return True
 
-	def to_dict(self):
-		"""
-		Return a dict equivalent of the attridict object
-		"""
-		return dict(self)
-
+    def to_dict(self):
+        """
+        Return a dict equivalent of the attridict object
+        """
+        return dict(self)
 
 
 if __name__ == "attridict":
-	import sys
-	sys.modules[__name__] = AttriDict
+    import sys
+
+    sys.modules[__name__].__class__ = AttriDict
